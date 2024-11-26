77464cc26ce4dc437095d1f7c1caaf0981155df7
Guido linux del <iguidoo@outlook.com>
2024-11-26 20:06:38 +0100

    initial commit

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../../apps/page-router/pages/*.{js,ts,jsx,tsx}',
+    '../../apps/pages-router/components/*.{js,ts,jsx,tsx}',
+    '../../apps/app-router/app/*.{js,ts,jsx,tsx}',
+    '../../apps/app-router/components/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/components/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'custom-color': '#674'
+      }
+    }
+  },
+  plugins: []
+}
