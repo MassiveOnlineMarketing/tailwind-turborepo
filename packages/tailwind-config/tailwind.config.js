2eb32894a868fbcb25504bd7b8e14e9fd6c01a9d
Guido linux del <iguidoo@outlook.com>
2024-11-26 21:16:27 +0100

    comment

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../apps/page-router/pages/*.{js,ts,jsx,tsx}',
-    '../../apps/pages-router/components/*.{js,ts,jsx,tsx}',
+    // '../../apps/page-router/pages/*.{js,ts,jsx,tsx}',
+    // '../../apps/pages-router/components/*.{js,ts,jsx,tsx}',
     '../../apps/app-router/app/*.{js,ts,jsx,tsx}',
     '../../apps/app-router/components/*.{js,ts,jsx,tsx}',
     '../../packages/ui/components/*.{js,ts,jsx,tsx}'
